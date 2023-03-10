2b883901b9829ed80918ba3c628921c80b69c1f8
KarolZawisza555 <karolzawisza555@gmail.com>
2023-03-10 23:18:36 +0100

    correct fix

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
 import { Icon } from '@iconify/vue';
 import { MotionPlugin } from '@vueuse/motion'
-import 'vue3-carousel/dist/carousel.css'
-import 'animate.css';
-import 'primevue/resources/themes/md-dark-indigo/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css ';
 import VueAnimXyz from '@animxyz/vue3'
-import '@animxyz/core'
 import PrimeVue from 'primevue/config';
 import Card from 'primevue/card';
 import Button from 'primevue/button';
 import Accordion  from 'primevue/accordion';
 import AccordionTab  from 'primevue/accordiontab';
 import Fieldset   from 'primevue/fieldset';
+import "./style.css";
+import 'vue3-carousel/dist/carousel.css'
+import 'animate.css';
+import 'primevue/resources/themes/md-dark-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css ';
+import '@animxyz/core'
 
 const app = createApp(App);
 app.use(VueAnimXyz)
